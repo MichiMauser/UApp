c68e588ba0d4a6f9276800abddb004067b5351e0
Ricu Razvan <razvanalexandru0204@gmail.com>
2025-01-09 01:40:04 +0200

    fixed image send

--- u-app/components/message.jsx
+++ u-app/components/message.jsx
@@ -1,30 +1,36 @@
-import Image from 'next/image'
+
 import classes from '../styles/ChatApp.module.css'
-import img from '../public/assets/img.png'
 import { useSelector } from 'react-redux'
 import { useEffect, useRef } from 'react'
 
-export default function Message({message}){
-    const {user} = useSelector((state) => state.user)
-    const {userCurrId, user: chat_user} = useSelector((state) => state.chat)
+export default function Message({ message }) {
+    const { user } = useSelector((state) => state.user)
+    const { userCurrId, user: chat_user } = useSelector((state) => state.chat)
 
     const ref = useRef()
     useEffect(() => {
-        ref.current?.scrollIntoView({behavior:"smooth"})
-    },[message])
+        ref.current?.scrollIntoView({ behavior: "smooth" })
+    }, [message])
 
+    console.log(message)
     return (
-     
-        <div ref={ref} className={`${classes.message} ${message.senderId === userCurrId &&  classes.owner}`}>
+
+        <div ref={ref} className={`${classes.message} ${message.senderId === userCurrId && classes.owner}`}>
             <div className={classes.messageInfo}>
                 <p>{message.senderId === userCurrId ? user && user.username : chat_user && chat_user.displayName}</p>
             </div>
             <div className={classes.messageContent}>
-                <p>{message.text}</p>
-                {message.img && <Image src={message.img} alt='message-pic' />}
+                {message.text && <p>{message.text}</p>}
+                {message.img && (
+                    <img
+                        src={message.img}
+                        alt="message-pic"
+                        style={{ maxWidth: '100%', height: 'auto' }}
+                    />
+                )}
             </div>
 
         </div>
-        )
+    )
 
 } 
--- u-app/lib/firebase.js
+++ u-app/lib/firebase.js
@@ -22,6 +22,6 @@ const firebaseConfig = {
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app)
+export const auth = getAuth()
 export const db = getFirestore(app)
 export const storage = getStorage(app)
